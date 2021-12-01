c5e8160f6686afe062aa1c8cef602d4e0ce520e6
dhiraj kahar <dhirajkahar7@gmail.com>
2021-12-01 17:58:01 +0530

    upadted

---
+++ utils/savefiles.js
@@ -0,0 +1,27 @@
+const fs = require('fs')  
+const axios = require('axios')
+const path = require('path')
+
+let saveFiles = {}
+
+saveFiles.downloadImage = async (inputs) => {  
+  const url = inputs.data.url
+  console.log("Download", url)
+  const location = path.join('./public/images/', String(inputs.filename))
+  const writer = fs.createWriteStream(location)
+
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  })
+
+  response.data.pipe(writer)
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve(true))
+    writer.on('error', reject(false))
+  })
+}
+
+module.exports = saveFiles
